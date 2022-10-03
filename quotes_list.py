e2d4c83c92281d6f7ddea873648554f92cc0c5a1
Patrick Repaci <patrickrepaci@hotmail.com>
2022-10-03 10:08:14 -0400

    removed data sorting issue

--- quotes_list.py
+++ quotes_list.py
@@ -11,7 +11,6 @@ quote_list = [
     "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
     "I attribute my success to this: I never gave or took any excuse.",
     "I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.",
-    "The most difficult thing is the decision to act, the rest is merely tenacity.",
     "Every strike brings me closer to the next home run.",
     "Definiteness of purpose is the starting point of all achievement.",
     "We must balance conspicuous consumption with conscious capitalism.",
